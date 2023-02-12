92e331035d73944b79db38437f42602e3faea7b2
Neus Garcia <neuspiano@gmail.com>
2023-02-12 14:08:36 +0100

    create AddButton component, add styling to button and albums list

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -1,6 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompactDisc } from "@fortawesome/free-solid-svg-icons";
+import styles from "../styles/AddButton.module.css";
 
 export const AddButton = () => {
   return (
-    <div>add album</div>
-  )
-}
+    <div className={styles.add_button}>
+      <FontAwesomeIcon className={styles.icon} icon={faCompactDisc} />
+      Add album
+    </div>
+  );
+};
